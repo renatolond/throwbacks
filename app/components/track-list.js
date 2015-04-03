fd2d1f2a41ee54f0984dc21c9fc5a7f7bdd77a43
Tamara Chahine <v0ld3m0r7@gmail.com>
2015-04-04 01:40:10 +1100

    Add 'send to spotify' button

--- app/components/track-list.js
+++ app/components/track-list.js
@@ -1,7 +1,8 @@
 var Backbone = require('backbone');
 var _ = require('underscore');
-var ItemView = require('../components/track-item');
+var ItemView = require('./track-item');
 var Collection = require('../collections/tracks');
+var Button = require('./button');
 
 module.exports =  Backbone.View.extend({
   tagName: 'ul',
@@ -21,17 +22,45 @@ module.exports =  Backbone.View.extend({
     var data = this.options.listData;
     var button = this.options.button;
 
-    var collection = new Collection({
+    this.collection = new Collection({
       url: '/from/' + data.from + '/to/' + data.to + '/for/' + data.user.id
     });
   
-    this.listenTo(collection, 'sync', function(data) {
+    this.listenTo(this.collection, 'sync', function(data) {
       _.each(data.models, _this.renderItem.bind(_this));
+      this.renderSendButton();
       button.trigger('end');
     });
 
-    // Render the view for the collection and then fetch it?
-    collection.fetch();
+    this.collection.fetch();
+  },
+
+  makeTrackset: function() {
+    console.log('Make trackset', this.collection);
+  },
+
+  renderSendButton: function() {
+    var _this = this;
+    var send = new Button({
+      className: 'send-tracks',
+      label: 'Send playlist to Spotify',
+      validate: function() {
+        // If the collection has models!
+        return true;
+      }
+    });
+
+    send.on('start', function() {
+      _this.sendToSpotify();
+    });
+
+    $(this.$el).append(send.el);
+  },
+
+  sendToSpotify: function() {
+    console.log('sendToSpotify');
+    // Get trackset
+    var trackset = this.makeTrackset();
 
   },
 
@@ -40,5 +69,7 @@ module.exports =  Backbone.View.extend({
       model: model
     });
     this.$el.append(item.el);
-  } 
+  }
+
+
 });
